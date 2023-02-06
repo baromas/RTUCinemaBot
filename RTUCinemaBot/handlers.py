8e287f9409251042011f0431ef9c4211a74b8897
Balandin Roman <baromas007@gmail.com>
2023-02-06 07:07:25 +0300

    Added second table to db, more methods, now supports  user recognition

--- RTUCinemaBot/handlers.py
+++ RTUCinemaBot/handlers.py
@@ -4,56 +4,79 @@ from aiogram.types import Message, InlineKeyboardButton
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from main import dp
-from sql import add
+from sql import add_record, add_user, get_full_name, get_room, check_user
 
 
 # reserved = False
 
 
+# FSM
+class FSMUserInfo(StatesGroup):
+    full_name = State()
+    room = State()
+
+
+class FSMBooking(StatesGroup):
+    date = State()
+    time = State()
+    list = State()
+
+
 # Start of programme
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: Message):
+    if await check_user(message.from_user.id):
+        await message.answer(text=f"Пользователь: {await get_full_name(message.from_user.id)}\n"
+                                  f"Номер комнаты: {await get_room(message.from_user.id)}")
+    else:
+        kb_add_user = [[types.KeyboardButton(text="Продолжить")]]
+        keyboard = types.ReplyKeyboardMarkup(
+            keyboard=kb_add_user,
+            resize_keyboard=True)
+        greetings = f"""Привет, {message.from_user.first_name}!
+Перед тем как приступить к работе, Вам необходимо внести короткую информацию о себе.
+Пожалуйста, нажмите кнопку "Продолжить" """
+        await message.answer(text=greetings, reply_markup=keyboard)
+
+
+# Add user to db
+@dp.message_handler(Text("Продолжить"))
+async def cmd_add_user(message: Message):
+    await FSMUserInfo.full_name.set()
+    await message.answer(
+        text="Введите своё ФИО",
+        reply_markup=types.ReplyKeyboardRemove(),
+    )
+
+
+@dp.message_handler(state=FSMUserInfo.full_name)
+async def set_full_name(message: Message, state: FSMContext):
+    await state.update_data(full_name=message.text)
+    await message.answer("Введите номер блока и комнаты")
+    await FSMUserInfo.next()
+
+
+@dp.message_handler(state=FSMUserInfo.room)
+async def set_room(message: Message, state: FSMContext):
+    await state.update_data(room=message.text)
+    data = await state.get_data()
     kb_reserve = [[types.KeyboardButton(text="Забронировать место в кинозале")]]
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=kb_reserve,
         resize_keyboard=True)
-    greetings = "Здравствуй, {}!".format(message.from_user.first_name)
-    await message.answer(text=greetings, reply_markup=keyboard)
+    await add_user(message.from_user.id, message.from_user.username, data['full_name'], data['room'])
+    await message.answer("Готово!", reply_markup=keyboard)
+    await state.finish()
 
 
-# await message.answer(text=message.from_user.id)
-# await message.answer(text=message.from_user.first_name)
-# if reserved:
-# await message.answer(text="У Вас уже есть забронированное место")
-# await state.clear()
-# global reserved
-# reserved = True
-
-#inkb_date = types.InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text="22.01", callback_data='22.01'))
-
-
-# FSM booking
-class FSMBooking(StatesGroup):
-    name = State()
-    date = State()
-    time = State()
-    seat = State()
-
-
+# Add record to db
 @dp.message_handler(Text("Забронировать место в кинозале"))
 async def cmd_book(message: Message):
-    await FSMBooking.name.set()
+    await FSMBooking.date.set()
     await message.answer(
-        text="Введите своё ФИО",
+        text="Выберите дату",
         reply_markup=types.ReplyKeyboardRemove(),
-        )
-
-
-@dp.message_handler(state=FSMBooking.name)
-async def set_name(message: Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    await message.answer("Выберите дату")
-    await FSMBooking.next()
+    )
 
 
 @dp.message_handler(state=FSMBooking.date)
@@ -66,14 +89,23 @@ async def set_date(message: Message, state: FSMContext):
 @dp.message_handler(state=FSMBooking.time)
 async def set_time(message: Message, state: FSMContext):
     await state.update_data(time=message.text)
-    await message.answer("Выберите место")
+    await message.answer("Пожалуйста, через запятую напишите список людей, которые пойдут с Вами в кинозал")
     await FSMBooking.next()
 
 
-@dp.message_handler(state=FSMBooking.seat)
-async def set_seat(message: Message, state: FSMContext):
-    await state.update_data(seat=message.text)
+@dp.message_handler(state=FSMBooking.list)
+async def set_list(message: Message, state: FSMContext):
+    await state.update_data(list=message.text)
     data = await state.get_data()
-    await add(data['name'], data['date'], data['time'], data['seat'])
+    await add_record(await get_full_name(message.from_user.id), data['date'], data['time'], data['list'])
     await message.answer("Бронирование успешно завершено")
     await state.finish()
+
+# await message.answer(text=message.from_user.id)
+# await message.answer(text=message.from_user.first_name)
+# if reserved:
+# await message.answer(text="У Вас уже есть забронированное место")
+# await state.clear()
+# global reserved
+# reserved = True
+# inkb_date = types.InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text="22.01", callback_data='22.01'))
--- RTUCinemaBot/sql.py
+++ RTUCinemaBot/sql.py
@@ -1,12 +1,50 @@
 import sqlite3
 
 
-async def add(full_name, date, time, seat):
+async def add_record(full_name, date, time, seat):
     connect = sqlite3.connect('booking.db')
     cursor = connect.cursor()
     cursor.execute('SELECT MAX(number) FROM records;')
     num = cursor.fetchone()[0] + 1
     m = [num, full_name, date, time, seat]
-    cursor.execute('INSERT INTO records (number, full_name, date, time, seat) VALUES (?,?,?,?,?)', m)
+    cursor.execute('INSERT INTO records (number, full_name, date, time, companions) VALUES (?,?,?,?,?)', m)
     connect.commit()
     cursor.close()
+
+
+async def add_user(user_id, user_name, full_name, room):
+    connect = sqlite3.connect('booking.db')
+    cursor = connect.cursor()
+    cursor.execute('SELECT MAX(number) FROM users;')
+    num = cursor.fetchone()[0] + 1
+    m = [num, user_id, user_name, full_name, room]
+    cursor.execute('INSERT INTO users (number, user_id, user_name, full_name, room) VALUES (?,?,?,?,?)', m)
+    connect.commit()
+    cursor.close()
+
+
+async def get_full_name(user_id):
+    connect = sqlite3.connect('booking.db')
+    cursor = connect.cursor()
+    name = cursor.execute(f"SELECT full_name FROM users WHERE user_id = '{user_id}'").fetchone()[0]
+    connect.commit()
+    cursor.close()
+    return name
+
+
+async def get_room(user_id):
+    connect = sqlite3.connect('booking.db')
+    cursor = connect.cursor()
+    name = cursor.execute(f"SELECT room FROM users WHERE user_id = '{user_id}'").fetchone()[0]
+    connect.commit()
+    cursor.close()
+    return name
+
+
+async def check_user(user_id):
+    connect = sqlite3.connect('booking.db')
+    cursor = connect.cursor()
+    exists = cursor.execute(f"SELECT EXISTS(SELECT user_id FROM users WHERE user_id ='{user_id}')").fetchone()[0]
+    connect.commit()
+    cursor.close()
+    return exists
